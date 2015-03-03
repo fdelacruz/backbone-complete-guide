e8707d44345ed04f5ab391575cd447c3e1a96a32
Francisco De La Cruz <helloquico@gmail.com>
2015-03-03 16:28:50 -0500

    Explain Views

--- js/main.js
+++ js/main.js
@@ -3,19 +3,18 @@ var Person = Backbone.Model.extend({
     name: 'Guest User',
     age: 23,
     occupation: 'Worker'
-  },
-
-  validate: function(attributes){
-    if (attributes.age < 0){
-      return 'Age must be positive.';
-    }
-
-    if (!attributes.name){
-      return 'Every person must have a name.';
-    }
-  },
-
-  work: function(){
-    return this.get('name') + ' is working.';
   }
 });
+
+var PersonView = Backbone.View.extend({
+  tagName: 'li',
+
+  initialize: function(){
+    this.render();
+  },
+
+  render: function(){
+    this.$el.html(this.model.get('name') + ' (' + this.model.get('age') + ') - ' + this.model.get('occupation'));
+  }
+});
+
