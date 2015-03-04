72612e27934a0109429c5159d64cd17881e6fdbe
Francisco De La Cruz <helloquico@gmail.com>
2015-03-03 23:28:20 -0500

    How to use namespace

--- js/main.js
+++ js/main.js
@@ -1,56 +1,72 @@
-var template = function(id) {
-  return _.template($('#' + id).html());
-};
+(function(){ 
 
-// Person Model
-var Person = Backbone.Model.extend({
-  defaults: {
-    name: 'Guest User',
-    age: 23,
-    occupation: 'Worker'
-  }
-});
+  window.App = {
+    Models: {},
+    Collections: {},
+    Views: {}
+  };
 
-// A list of People
-var PeopleCollection = Backbone.Collection.extend({
-  model: Person
-});
+  window.template = function(id) {
+    return _.template($('#' + id).html());
+  };
 
-// View for all People
-var PeopleView = Backbone.View.extend({
-  tagName: 'ul',
-  render: function(){
-    this.collection.each(function(person){
-      var personView = new PersonView({ model: person });
-      this.$el.append(personView.render().el);
-    }, this);
-    return this;
-  }
-});
+  // Person Model
+  App.Models.Person = Backbone.Model.extend({
+    defaults: {
+      name: 'Guest User',
+      age: 23,
+      occupation: 'Worker'
+    }
+  });
 
-// The View for a Person
-var PersonView = Backbone.View.extend({
-  tagName: 'li',
-  template: template('personTemplate'),
-  render: function(){
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;
-  }
-});
+  // A list of People
+  App.Collections.People = Backbone.Collection.extend({
+    model: App.Models.Person
+  });
 
-var peopleCollection = new PeopleCollection([
-    {
-      name: 'Mohit Jain',
-      age: 26
-    },
-    {
-      name: 'Taroon Tyagi',
-      age: 25,
-      occupation: 'Web Designer'
-    },
-    {
-      name: 'Rahul Narang',
-      age: 26,
-      occupation: 'Java Developer'
-    }  
-]);
+  // View for all People
+  App.Views.People = Backbone.View.extend({
+    tagName: 'ul',
+
+    render: function(){
+      this.collection.each(function(person){
+        var personView = new App.Views.Person({ model: person });
+        this.$el.append(personView.render().el);
+      }, this);
+
+      return this;
+    }
+  });
+
+  // The View for a Person
+  App.Views.Person = Backbone.View.extend({
+    tagName: 'li',
+
+    template: template('personTemplate'),
+
+    render: function(){
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+
+  var peopleCollection = new App.Collections.People([
+      {
+        name: 'Mohit Jain',
+        age: 26
+      },
+      {
+        name: 'Taroon Tyagi',
+        age: 25,
+        occupation: 'Web Designer'
+      },
+      {
+        name: 'Rahul Narang',
+        age: 26,
+        occupation: 'Java Developer'
+      }  
+  ]);
+
+  var peopleView = new App.Views.People({ collection: peopleCollection });
+  $(document.body).append(peopleView.render().el)
+})();
